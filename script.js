0a49feed74a11ca9cbdcb8dd667ecb3ad9a06b76
Tiago S. Hucs <tiagohucs@gmail.com>
2025-11-20 16:43:03 -0300

    cadastro de itens

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const msgTemplate = document.getElementById('msgTemplate');
 const poductListTemplate = document.getElementById('poductListTemplate');
 
+const itemFormTemplate = document.getElementById('itemFormTemplate');
+const itemListTemplate = document.getElementById('itemListTemplate');
+const itemRowTemplate = document.getElementById('itemRowTemplate');
+
 const msgContent = document.getElementById('msg-content');
 const mainContent = document.getElementById('main-content');
+
+let store = {
+    itens: [],
+    products: []
+}
 
 function sendMessage(textMsg) {
     let newContent = msgTemplate.innerHTML
@@ -11,9 +20,32 @@ function sendMessage(textMsg) {
     msgContent.innerHTML = newContent;
 }
 
-function renderBuyList() {
+function renderBuyForm() {
     clearAll();
-    sendMessage('Lista de compras acessada');
+    sendMessage('Cadastrar um item para confecção de produtos');
+    let newContent = itemFormTemplate.innerHTML;
+    mainContent.innerHTML = newContent;
+}
+
+function renderItemList() {
+  // limpa conteúdo
+  mainContent.innerHTML = '';
+
+  // clona a estrutura da tabela
+  const tableClone = itemListTemplate.content.cloneNode(true);
+  const tbody = tableClone.querySelector('tbody');
+
+  // para cada item, clona a linha e preenche células
+  store.itens.forEach(i => {
+    const rowClone = itemRowTemplate.content.cloneNode(true);
+    rowClone.querySelector('.cell-name').textContent = i.name;
+    rowClone.querySelector('.cell-price').textContent = i.price;
+    rowClone.querySelector('.cell-unity').textContent = i.unity;
+    tbody.appendChild(rowClone);
+  });
+
+  // insere a tabela pronta no DOM
+  mainContent.appendChild(tableClone);
 }
 
 function renderMaterialList() {
@@ -28,15 +60,29 @@ function renderProductList() {
     mainContent.innerHTML = newContent;
 }
 
-function clearMessage(){
+function clearMessage() {
     msgContent.innerHTML = null;
 }
 
-function clearContent(){
+function clearContent() {
     mainContent.innerHTML = null;
 }
 
-function clearAll(){
+function clearAll() {
     clearMessage();
     clearContent();
+}
+
+// actions 
+
+function createItem(event) {
+    event.preventDefault(); // evita recarregar a página
+    const item = {
+        name: document.getElementById("nome").value,
+        price: parseFloat(document.getElementById("preco").value),
+        unity: document.getElementById("unidade").value
+    };
+    store.itens.push(item)
+    renderItemList();
+    //alert("Item criado:\n" + JSON.stringify(item, null, 2));
 }
